28e0834f657a2c8eb9c890ee79bdb8a13eba38ab
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-04-05 06:34:16 +0800

    minor adjustments/modifications (make transaction page and checkbox component)

--- components/CheckboxComponent.tsx
+++ components/CheckboxComponent.tsx
@@ -1,6 +1,6 @@
-import { IconSquareCheckFilled, IconSquareFilled } from "@tabler/icons-react-native";
 import React, { useEffect } from "react";
 import { Pressable } from "react-native";
+import Svg, { Path, Rect } from "react-native-svg";
 
 type Props = {
     isChecked: boolean;
@@ -22,12 +22,29 @@ export default function CheckboxComponent({ isChecked, onPress }: Props) {
 
 
     return (
-        <Pressable onPress={onToggle} style={{ padding: 8 }}>
-            {checked ? (
-                <IconSquareCheckFilled size={24} color="#ffffff" />
-            ) : (
-                <IconSquareFilled size={24} color="#fcfcfc" />
-            )}
+        <Pressable onPress={onToggle} style={{ padding: 6 }}>
+            <Svg width={23} height={23} viewBox="0 0 24 24">
+                <Rect
+                    x="3"
+                    y="3"
+                    width="18"
+                    height="18"
+                    rx="4"
+                    ry="4"
+                    fill="#D9D9D9"
+                    stroke="#411C0E"
+                    strokeWidth={1}
+                />
+                {checked && (
+                    <Path
+                        d="M9 12l2 2 4-4"
+                        fill="none"
+                        stroke="#411C0E"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                    />
+                )}
+            </Svg>
         </Pressable>
     );
 }
