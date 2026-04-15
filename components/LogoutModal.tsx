e3363e4fdce69f4e01bed9e26e7720c87bf821cc
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-04-15 18:39:23 +0800

    logout modal w/ backdrop updated

--- components/LogoutModal.tsx
+++ components/LogoutModal.tsx
@@ -7,52 +7,66 @@ type Props = {
 }
 export default function LogoutModal({onConfirm, onCancel, isLoading}: Props) {
     return (
-        <View style={styles.container}>
-            <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 10, color: '#5c3406'}}>Confirm Logout</Text>
-            <Text style={styles.message}>Are you sure you want to logout?</Text>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.confirmbutton} onPress={onConfirm}>
-                    <Text style={styles.text}>{isLoading ? "Logging out..." : "Logout"}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cancelbutton} onPress={onCancel}>
-                    <Text style={styles.text}>Cancel</Text>
-                </TouchableOpacity>
+        <View style={styles.backdrop}>
+            <View style={styles.container}>
+                <Text style={{fontSize: 20, fontWeight: 'bold', marginVertical: 8, color: '#5c3406'}}>Confirm Logout</Text>
+                <Text style={styles.message}>Are you sure you want to logout?</Text>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.confirmbutton} onPress={onConfirm}>
+                        <Text style={styles.text}>{isLoading ? "Logging out..." : "Logout"}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cancelbutton} onPress={onCancel}>
+                        <Text style={styles.text}>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    backdrop: {
         position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#411C0E80',
+    },
+    container: {
         backgroundColor: '#fff',
+        width: '90%',
         padding: 20,
         borderRadius: 45,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: -30,
     },
     message: {
         fontSize: 16,
-        marginBottom: 20,
+        marginBottom: 25,
+        color: '#411C0E'
     },
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        gap: 10,
-        width: '100%',
+        gap: 15,
+        width: '97%',
     },
     confirmbutton: {
         width: '40%',
         alignItems: 'center',
-        backgroundColor: 'red',
+        backgroundColor: '#B00B0BCC',
         padding: 10,
         borderRadius: 45,
     },
     cancelbutton: {
         width: '40%',
         alignItems: 'center',
-        backgroundColor: 'grey',
+        backgroundColor: '#565656CC',
         padding: 10,
         borderRadius: 45,
     },
