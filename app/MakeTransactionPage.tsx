1a5b27bc693c5922e73388fa5c5010cc919274a1
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-04-23 03:05:12 +0800

    search icon margin ADJUSTED

--- app/MakeTransactionPage.tsx
+++ app/MakeTransactionPage.tsx
@@ -355,7 +355,7 @@ export default function MakeTransactionPage() {
                     onChangeText={setSearchTerm}
                     style={{ flex: 1, marginLeft: 10 }}
                 />
-                <Svg width="25" height="25" viewBox="0 0 25 25" fill="none">
+                <Svg width="25" height="25" viewBox="0 0 25 25" fill="none" style={{ margin: 5 }}>
                     <Path d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" 
                         stroke="#411C0E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
                 </Svg>
